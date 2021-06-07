33f328b6349384cf828036c60d1756ede8e5a6d2
Lewis Jang <lewis.aedes@gmail.com>
2021-06-07 19:31:44 +0800

    init part file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PlayComponent } from './play/play.component';
+import { SettingComponent } from './setting/setting.component';
+import { MenuComponent } from './menu/menu.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayComponent,
+    SettingComponent,
+    MenuComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
